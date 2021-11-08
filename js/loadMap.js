4fae5b67cc627efb5afa18adf8dc48321ea24747
EpicPlum <willem.kupec@gmail.com>
2021-11-08 14:39:05 +0100

    added geojson file

--- js/loadMap.js
+++ js/loadMap.js
@@ -1,6 +1,6 @@
 var karte = L.map('karte').setView([20, 0], 2);
 
-$.getJSON('https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson', function(data)
+$.getJSON('/img/map.geojson', function(data)
 {
   L.geoJson(data).addTo(karte);
 });
