2a7101b70fe879eb610ea8bbb4c065ec35262300
Karel <karelkarunia24@gmail.com>
2021-12-13 14:04:58 +0100

    latet

--- db/seeds/01_users.ts
+++ db/seeds/01_users.ts
@@ -1,12 +1,17 @@
 import { Knex } from "knex";
+import bcrypt from "bcrypt";
+
 
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex("expenses").del();
+  const salt = await bcrypt.genSalt();
+  const karel1passwordHash = await bcrypt.hash('karel1', salt);
+  const adminpasswordHash = await bcrypt.hash('admin', salt);
 
   // Inserts seed entries
   await knex("expenses").insert([
-    { email: 'karelkarunia24@gmail.com', password: 'karel1' },
-    { email: 'admin@ta', password: 'admin' },
+    { email: 'karelkarunia24@gmail.com', password: karel1passwordHash },
+    { email: 'admin@ta', password: adminpasswordHash },
   ]);
 };
--- ts/server.ts
+++ ts/server.ts
@@ -9,7 +9,7 @@ import { knex as knexDriver } from "knex";
 import config from "../knexfile";
 
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const knex = knexDriver(config);
 const authService = new AuthService()
