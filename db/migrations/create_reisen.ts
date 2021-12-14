a2741cfad0c060cd1b2a67000db8a1b97f2a9b64
Karel <karelkarunia24@gmail.com>
2021-12-15 00:00:57 +0100

    added back create_reisen for migrate rollback

---
+++ db/migrations/create_reisen.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema
+        .createTable('reisen', function (table) {
+            table.uuid('id').primary();
+            table.string('name', 255).notNullable();
+            table.date('startDatum').notNullable();
+            table.date('endDatum').notNullable();
+            table.string('land').notNullable();
+            table.timestamp('created_at').defaultTo(knex.fn.now());
+        })
+};
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema
+        .dropTableIfExists('reisen')
+}
