16b1b8f805c59ae86a8fd6df706d4e61915bbab4
Karel <karelkarunia24@gmail.com>
2021-12-15 01:13:24 +0100

    changed id back to uuid

--- db/migrations/20211129022318_create_users.ts
+++ db/migrations/20211129022318_create_users.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema
-        .createTableIfNotExists('users', function (table) {
+        .createTable('users', function (table) {
             table.string('email').notNullable();
             table.string('password').notNullable();
             table.timestamp('created_at').defaultTo(knex.fn.now());
