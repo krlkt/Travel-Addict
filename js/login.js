e293ba22048b08ea4bf44d04509e52d689b6e4c4
Karel <karelkarunia24@gmail.com>
2021-12-20 16:01:03 +0100

    dynamic url

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ const email = document.querySelector('input[id="email"]');
 const password = document.querySelector('input[id="password"]');
 
 // const BASE_URL = "http://localhost:8080";
-const BASE_URL = "https://travel-addict-backend-server.herokuapp.com";
+// const BASE_URL = "https://travel-addict-backend-server.herokuapp.com";
 const BASE_URL = process.env.NODE_ENV === "production" ? "https://travel-addict-backend-server.herokuapp.com" : "http://localhost:8080";
 
 function afterInput(e) {
