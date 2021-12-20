2e2722fc3bf094f395b52b4cd04c6616ea91b936
Karel <karelkarunia24@gmail.com>
2021-12-20 03:15:20 +0200

    fixing cors error

--- ts/server.ts
+++ ts/server.ts
@@ -21,7 +21,7 @@ app.options("/*", function (req, res, next) {
     if (allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
-    // res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -30,6 +30,14 @@ app.options("/*", function (req, res, next) {
 
 app.use(express.json())
 app.use(cookieParser());
+
+
+app.use(
+    cors({
+        origin: 'https://travel-addict.netlify.app/',
+        credentials: true
+    })
+);
 
 // install middleware (open api validator)
 app.use(
@@ -40,12 +48,6 @@ app.use(
     })
 );
 
-app.use(
-    cors({
-        origin: ['http://127.0.0.1:5500', 'https://travel-addict.netlify.app/'],
-        credentials: true
-    })
-);
 
 // app.options('/login', cors()) // enable pre-flight request for login post request
 
